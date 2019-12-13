2f363cdffceaf818ac9742b4629a0d7fb1132420
Capt-Lance <bryonnush@gmail.com>
2019-12-12 19:59:11 -0600

    initial checkin

---
+++ ScheduledJobTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Topshelf;
+
+namespace ScheduledJobTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rc = HostFactory.Run(x =>
+            {
+                x.Service<SchedulingService>(s =>
+                {
+                    s.ConstructUsing(name => new SchedulingService());
+                    s.WhenStarted(ss => ss.StartAsync().Wait());
+                    s.WhenStopped(ss => ss.StopAsync());
+                });
+            });
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
